d4aa7805db2bad5e3854015222c19cfb5f7d07a0
fadhel-rizanda <fadhelrizanda@gmail.com>
2026-01-06 09:04:32 +0700

    fix: gemini lint

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import Image from "next/image";
-import { Icon } from '@iconify/react';
-import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Icon icon="ph:heart" className="opacity-50 h-4 w-4" />
-        <Button variant="outline">Button</Button>
         <Image
           className="dark:invert"
           src="/next.svg"

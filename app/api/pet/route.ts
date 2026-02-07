ad10d37e7dcd2edeb9124b9e8040d0d6dc0c0e3a
Khalidfatur87 <khalidfe87@gmail.com>
2026-02-07 19:27:27 +0700

    Update variable url

--- app/api/pet/route.ts
+++ app/api/pet/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const API_URL = process.env.API_URL  || "http://localhost:8000"
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"
 
 // Whitelist of allowed query parameters
 const ALLOWED_PARAMS = [

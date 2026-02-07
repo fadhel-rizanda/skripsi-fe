2bd9c42b7a5af098d18dcc295697777cae08d0bd
Khalidfatur87 <khalidfe87@gmail.com>
2026-02-07 17:38:19 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["eksplore-skripsi.s3.ap-southeast-2.amazonaws.com"],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'eksplore-skripsi.s3.ap-southeast-2.amazonaws.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;

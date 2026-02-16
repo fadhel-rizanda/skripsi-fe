a3a4422eab661fbeb5e139f72a8ed71f83996582
Khalidfatur87 <khalidfe87@gmail.com>
2026-02-16 13:00:54 +0700

    update

--- app/(main)/create-update-pet/page.tsx
+++ app/(main)/create-update-pet/page.tsx
@@ -111,7 +111,7 @@ export default function RehomePetForm() {
       });
       setProfileFiles(
         (pet.profile_pictures || []).map((p: any) => ({
-          name: p.filename || p.public_url || "Existing Image",
+          name: p.filename || "Existing Image",
           size: "-",
         }))
       );

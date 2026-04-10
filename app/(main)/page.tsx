6c95ea73713a1064bc7b02503319944e559286fa
Khalidfatur87 <khalidfe87@gmail.com>
2026-04-10 23:00:27 +0700

    update remove wave

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -60,12 +60,6 @@ export default function Home() {
                         </div>
                     </div>
 
-                    {/* SVG Wave Divider at the bottom of hero */}
-                    <div className="absolute bottom-0 w-full overflow-hidden leading-[0]">
-                        <svg className="relative block w-[calc(100%+1.3px)] h-[50px] sm:h-[80px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V120H0V95.8C59.71,118.08,130.83,119.5,192.71,105.7,238.16,95.53,281.32,73.49,321.39,56.44Z" className="fill-gray-50"></path>
-                        </svg>
-                    </div>
                 </section>
 
                 {/* Why Pawsitive Section */}
